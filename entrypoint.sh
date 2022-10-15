8ad12d45909b1138de29faf050e8d99908aea147
dvkos <115855909+dvkos@users.noreply.github.com>
2022-10-15 02:49:18 -0700

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# config xray
+cat << EOF > /etc/config.json
+{
+  "inbounds":[
+    {
+      "port": 8888,
+      "protocol": "vless",
+      "settings": {
+        "decryption": "none",
+        "clients": [
+          {
+            "id": "77cbf8fb-49cb-4a8f-838d-293f7b622f4d"
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "ws"
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom"
+    }
+  ]
+}	
+EOF
+
+# run xray
+/usr/bin/xray run -config /etc/config.json
